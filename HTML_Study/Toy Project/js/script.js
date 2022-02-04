8ca91cfcf84bd7889d5df554a3a1410426c9290f
Haru-arp <vr1997@naver.com>
2022-02-04 21:37:12 +0900

    Toy Project

---
+++ HTML_Study/Toy Project/js/script.js
@@ -0,0 +1,42 @@
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      modeToggle = document.querySelector(".dark-light"),
+      searchToggle = document.querySelector(".searchToggle"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+      
+      let getMode = localStorage.getItem("mode");
+          if(getMode && getMode === "dark-mode"){
+              body.classList.add("dark");
+
+          }
+      
+    // Js code to toggle dark and light mode
+      modeToggle.addEventListener("click", () =>{
+          modeToggle.classList.toggle("active");
+          body.classList.toggle("dark");
+
+          if(!body.classList.contains("dark")){
+              localStorage.setItem("mode","light-mode");
+          }else{
+              localStorage.setItem("mode","dark-mode");
+          }
+          
+      })
+
+    // Js code to toggle search box
+        searchToggle.addEventListener("click", () =>{
+            searchToggle.classList.toggle("active");
+    })      
+
+    //js code to toggle sidebar 
+    sidebarOpen.addEventListener("click", () =>{
+        nav.classList.add("active")
+    })
+
+    body.addEventListener("click", e =>{
+        let clickedElm = e.target;
+
+        if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu"))
+            nav.classList.remove("active");
+    })
