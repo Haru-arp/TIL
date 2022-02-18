7431806766078b17424ac2defea7ad1c77b2c066
Haru-arp <vr1997@naver.com>
2022-02-18 23:33:51 +0900

    push

--- Algorithm/SWEA/5432_쇠막대기자르기/연습 풀이 파일.py
+++ Algorithm/SWEA/5432_쇠막대기자르기/연습 풀이 파일.py
@@ -5,6 +5,19 @@ sys.stdin = open('input.txt')
 T = int(input())
 
 for tc in range(1, T + 1):
+    cnt = sol = 0
+    lst = input()
+
+    for i in range(0, len(lst)):
+        if lst[i] == '(':
+            cnt += 1
+        else:
+            if lst[i-1] == "(":
+                cnt -= 1
+                sol += cnt
+            else:
+                cnt -= 1
+                sol += 1
 
 
 
